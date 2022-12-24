d7475d2971f26a79b5ad0e8051a13f86de28c086
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-12-24 17:27:56 +0700

    update: change smtp to gmail

--- src/helpers/mail.helper.js
+++ src/helpers/mail.helper.js
@@ -1,8 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const emailService = nodemailer.createTransport({
-  host: process.env.CONFIG_SMTP_HOST,
-  port: process.env.CONFIG_SMTP_PORT,
+  service: process.env.CONFIG_SMTP_SERVICE,
   auth: {
     user: process.env.CONFIG_SMTP_EMAIL,
     pass: process.env.CONFIG_SMTP_PASSWORD,
